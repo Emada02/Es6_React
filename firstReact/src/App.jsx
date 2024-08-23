643b963bab20b3d6b50c6ff4c3d8b2789bfcaf1c
Emada Nipolli <emadanipolli@gmail.com>
2024-08-23 12:53:50 +0200

    firstCommit

---
+++ firstReact/src/App.jsx
@@ -0,0 +1,13 @@
+// Create a new React app which says: Hello world, this is my first React app.(http://localhost:5173/)
+function App() {
+
+  return (
+    <>
+     <h1>Hello World!</h1>
+     <h2>This is my first React app✅</h2>
+    </>
+  )
+}
+
+export default App
+
